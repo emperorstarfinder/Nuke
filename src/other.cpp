419ffd193ff11de98fe62ac334e88371de4e1430
robclu <robclu1818@gmail.com>
2015-04-14 23:50:24 +0200

    Added pretty rubbish fog =/

--- src/other.cpp
+++ src/other.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>			// Allows standard io
 #include <stdlib.h>			// Needed for malloc and free
-#include <unistd.h>			// Sw that the sleep function can be called
+#include <unistd.h>			// So that the sleep function can be called
 #include <GL/glut.h>		// Header for the GLUT library
 #include <GL/gl.h>			// Header for the OpenGL32 library
 #include <GL/glut.h>		// Header for the GLu32 library
@@ -19,25 +19,20 @@ int light;					// Lighting (1 = ON, 0 = OFF)
 int lp;						// If 'L' is pressed
 int fp;						// If 'F' is pressed
 
-GLfloat rotX, rotY, rotZ;			// Angles and speeds of rotation
-GLfloat z = -5.0f;					// Depth into the screen
-GLuint texture[1];					// Storage for 1 texture
+GLfloat rotX, rotY, speedX, speedY;			// Angles and speeds of rotation
+GLfloat z = -5.0f;							// Depth into the screen
+
+GLfloat lightAmbient[]  = {0.5f, 0.5f, 0.5f, 1.0f};	// White ambient light half density
+GLfloat lightDiffuse[]  = {1.0f, 1.0f, 1.0f, 1.0f};	// Really bright white light
+GLfloat lightPosition[] = {0.0f, 0.0f, 2.0f, 1.0f};	// Out the screen facing front face
+
+GLuint fogMode[] = {GL_EXP, GL_EXP2, GL_LINEAR};	// Storage for the fog types
+GLuint fogfilter = 0;								// Which type of fog to use
+GLfloat fogColor[4] = {0.5f, 0.5f, 0.5f, 1.0f};		// Fog color
+
+GLuint filter;						
+GLuint texture[1];					// Storage for 1 textures
 GLuint blend;						// Turn the blending on or off
-
-GLuint cube;						// Storage for the display list
-GLuint top;							// Storage for the second display list
-GLuint xloop;						// Loop for the x axis
-GLuint yloop;						// Loop for the y axis 
-
-// Define colors for the boxes
-static GLfloat boxcol[5][3] = {
-	{1.0f,0.0f,0.0f},{1.0f,0.5f,0.0f},{1.0f,1.0f,0.0f},{0.0f,1.0f,0.0f},{0.0f,1.0f,1.0f}
-};
-
-// Define colors for the top of the boxes
-static GLfloat topcol[5][3] = {
-	{.5f,0.0f,0.0f},{0.5f,0.25f,0.0f},{0.5f,0.5f,0.0f},{0.0f,0.5f,0.0f},{0.0f,0.5f,0.5f}
-};
 
 // Define an image type 
 typedef struct Image {
@@ -45,88 +40,6 @@ typedef struct Image {
 	unsigned long sizeY;
 	char *data;
 } Image;
-
-// Function for building a list
-GLvoid buildLists() 
-{
-	cube = glGenLists(2);			// 2 lists and return pointer to the first
-	glNewList(cube, GL_COMPILE);	// Store the lists at cueb (location) and compile
-
-	// Cube without the top
-	glBegin(GL_QUADS);	
-	
-	// Bottom face
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f( 1.0f, -1.0f, -1.0f);	// Top Left Of The Texture and Quad
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
-    
-    // Front Face
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
-    
-    // Back Face
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
-    
-    // Right face
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
-    
-    // Left Face
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
-    glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
-
-	glEnd();
-	glEndList();
-
-	// Move the to the mem location of the second list
-	top = cube + 1;
-	glNewList(top, GL_COMPILE);			// Generate the second list 
-
-	glBegin(GL_QUADS);	
-
-	// Top face
-	glTexCoord2f(0.0f, 1.0f); 
-    glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
-    glTexCoord2f(0.0f, 0.0f); 
-    glVertex3f(-1.0f,  1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0f, 0.0f); 
-    glVertex3f( 1.0f,  1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0f, 1.0f); 
-    glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
-		
-	glEnd();
-	glEndList();
-}
 
 // Other bitmap loader not working
 static unsigned int getint(FILE *fp)
@@ -242,35 +155,44 @@ void loadGlTextures()
 		exit(0);											// Quit
 	}
 	
-	const char * textureFile = "textures/Cube.bmp";		// Declare the filename
+	const char * textureFile = "textures/Crate.bmp";		// Declare the texture filename
 
 	if (!loadImage(textureFile, image1)) {
 		exit(1);
 	}
 
-	glGenTextures(1, &texture[0]);							// Create the texture
-	glBindTexture(GL_TEXTURE_2D, texture[0]);				// 2D texture (x, y)
+	// One texture
+	glGenTextures(1, &texture[0]);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);					// Scale cheaply when img bigger than texture
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);	// Scale cheaply when img smaller than texture
+	// Create a mipmapped texture
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);	// Mipmapped
+	
+	// Build the mipmapped texture, params are:
+	//	[2D texture, 3 colors, wifth, height, RGB, byte type data, actual data]
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, image1->sizeX, image1->sizeY, GL_RGB, GL_UNSIGNED_BYTE, image1->data);
 
-	// Params are:
-	//	[2d tex  ,  detail level     ,  num components, img x size, img y size, border,
-	//	 col data, unsigned byte data, data itself]
-    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, image1->sizeX, image1->sizeY, GL_RGB, GL_UNSIGNED_BYTE, image1->data);
+	// Free the image 
+	if (image1) {
+		if (image1->data) {
+			free(image1->data);
+		}
+		free(image1);
+	}
 }
 
 // General OpenGL init. Sets all initial params
 void initGL(int width, int height)
 {
 	loadGlTextures();							// Load the textures
-	buildLists();								// Build the lists
 	glEnable(GL_TEXTURE_2D);					// Enable texture mapping
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// Black background
 	glClearDepth(1.0);							// Allows depth buffer to be cleared
 	glDepthFunc(GL_LESS);						// The type of depth test
 	glEnable(GL_DEPTH_TEST);					// Enables depth testing
 	glShadeModel(GL_SMOOTH); 					// Enables smoothe color shading 
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
 	glMatrixMode(GL_PROJECTION);				// Type of GL Matrix
 	glLoadIdentity();							// Reset the projection matrix
@@ -281,9 +203,19 @@ void initGL(int width, int height)
 	glMatrixMode(GL_MODELVIEW);					// Back to model view matrix
 
 	// Setup a light
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_COLOR_MATERIAL);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, lightAmbient);	// Add ambient light
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiffuse);	// Add diffusion light
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPosition);	// Set the light position
+	glEnable(GL_LIGHT1);
+
+	// Setup the fog
+	glFogi(GL_FOG_MODE, fogMode[fogfilter]);       // Fog Mode
+	glFogfv(GL_FOG_COLOR, fogColor);               // Set Fog Color
+	glFogf(GL_FOG_DENSITY, 0.35f);                 // How Dense Will The Fog Be
+	glHint(GL_FOG_HINT, GL_DONT_CARE);			       // Fog Hint Value
+	glFogf(GL_FOG_START, 4.0f);                    // Fog Start Depth
+	glFogf(GL_FOG_END, 9.0f);                      // Fog End Depth
+	glEnable(GL_FOG);                              // Enables GL_FOG	
 }
 
 // Resize window if (for some reason) the size is changed
@@ -308,26 +240,64 @@ void drawGlScene()
 	// Clear the color and the depth buffers 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 	
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	// Reset the view
+	glLoadIdentity();						// Reset the vie
+	glTranslatef(0.f, 0.0f, z);				// Move z units along z axis
+	glRotatef(rotX, 1.0f, 0.0f, 0.0f);		// Rotate x-axis
+	glRotatef(rotY, 0.0f, 1.0f, 0.0f);		// Rotate y-axis
 
-	// Make a pyramid like structure from cubes
-	for (yloop = 1; yloop < 6; yloop++) {
-		for (xloop = 0; xloop < yloop; xloop++) {
-			glLoadIdentity();
+	glBindTexture(GL_TEXTURE_2D, texture[filter]);	// Select the texture to use
 
-			glTranslatef(1.4f+(((float)xloop)*2.8f)-(((float)yloop)*1.4f),((6.0f-((float)yloop))*2.4f)-7.0f,-20.0f);
+	// Draw a square 
+	glBegin(GL_QUADS);					// Start drawing a quad
 
-			glRotatef(45.0f - (2.0f*yloop)+rotX, 1.0f, 0.0f, 0.0f);
-			glRotatef(45.0f + rotY, 0.0f, 1.0f, 0.0f);
-
-			glColor3fv(boxcol[yloop-1]);	    
-			glCallList(cube);
-	    
-			glColor3fv(topcol[yloop-1]);
-			glCallList(top);
-		}
-	}
+	 // Front Face (note that the texture's corners have to match the quad's corners)
+    glNormal3f( 0.0f, 0.0f, 1.0f);                              // front face points out of the screen on z.
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+    
+    // Back Face
+    glNormal3f( 0.0f, 0.0f,-1.0f);                              // back face points into the screen on z.
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
 	
+    // Top Face
+    glNormal3f( 0.0f, 1.0f, 0.0f);                              // top face points up on y.
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+    
+    // Bottom Face       
+    glNormal3f( 0.0f, -1.0f, 0.0f);                             // bottom face points down on y. 
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+    
+    // Right face
+    glNormal3f( 1.0f, 0.0f, 0.0f);                              // right face points right on x.
+    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+    
+    // Left Face
+    glNormal3f(-1.0f, 0.0f, 0.0f);                              // left face points left on x.
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+	
+	glEnd();							// Finished drawing the quad
+
+	rotX += speedX;
+	rotY += speedY;
+
 	// Double buffered so swap the buffers 
 	// to display what was just drawn
 	glutSwapBuffers();
@@ -344,37 +314,84 @@ void keyPressed(unsigned char key, int x, int y)
 			exit(0);
 			break;
 
-			default:
+		case 76:
+		case 108:							// Switch the lighting
+			light = light ? 0 : 1;			// Change light variable
+			if (!light) {
+				glDisable(GL_LIGHTING);		
+			}
+			else {
+				glEnable(GL_LIGHTING);
+			}
+			break;
+
+		case 70:
+		case 102:							// Switch the filter
+			filter +=1;
+			if (filter > 2) {				// Out of range
+				filter = 0;					// back to first filter
+			}
+			break;
+
+		case 'g':
+		case 'G':
+			fogfilter += 1;
+			if (fogfilter > 2) {
+				fogfilter = 0;
+			}
+			glFogi(GL_FOG_MODE, fogMode[fogfilter]);
+			break;
+
+		case 98:
+		case 110:							// Switch the blending
+			blend = blend ? 0 : 1;			// Toggle current blend value
+			if (!blend) {
+				glDisable(GL_BLEND);		// Turn off blending
+				glEnable(GL_DEPTH_TEST);	// Turn on depth testing
+			}
+			else {
+				glEnable(GL_BLEND);			// Turn on blending
+				glDisable(GL_DEPTH_TEST);	// Turn off depth testing
+			}
+			break;
+
+		default:
 			break;
 	}
 }
 
-// Function for dealing with the movement
+// Function to check for movement 
 void specialKeyPressed(int key, int x, int y)
 {
-	// Aviod thrashing the key
-	usleep(100);
+	usleep(100);						// Again no funny behaviour
 
-	switch (key) {
+	switch(key) {
+		case GLUT_KEY_PAGE_UP:
+			z -= 0.2f;
+			break;
+
+		case GLUT_KEY_PAGE_DOWN:
+			z += 0.2f;
+			break;
+
 		case GLUT_KEY_UP:
-				rotX -= 1.f;
-				break;
-				
+			speedX += 0.1f;
+			break;
+
 		case GLUT_KEY_DOWN:
-				rotX += 1.f;
-				break;
+			speedX -= 0.1f;
+			break;
 
 		case GLUT_KEY_LEFT:
-				rotY -= 1.f;
-				break;
+			speedY -= 0.1f;
+			break;
 
-		case GLUT_KEY_RIGHT:
-				rotY += 1.f;		
-				break;
+		case GLUT_KEY_RIGHT: 
+			speedY += 0.1f;
+			break;
 
 		default:
-				printf("Special key pressed, but no action yet");
-				break;
+			break;
 	}
 }
 
@@ -412,7 +429,7 @@ int main(int argc, char **argv)
 	// Register the keyboard checking function
 	glutKeyboardFunc(&keyPressed);
 
-	// Check for the special keys
+	// Check the movement functions 
 	glutSpecialFunc(&specialKeyPressed);
 	
 	// Init GL - the window
