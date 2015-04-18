f702422fcab70e2263d0bec5e950e888ef8f62dc
robclu <robclu1818@gmail.com>
2015-04-19 00:46:41 +0200

    added some drawing functionality.

--- src/renderer/draw.hpp
+++ src/renderer/draw.hpp
@@ -25,33 +25,34 @@
 
 namespace nuke {
 	namespace rend {
-	/** Function that draws an OpenGL object to the screen.
-	 * Currently this is slow and badly designed but I'm going for functional
-	 * over performance to start.
-	 */
-	template <class DrawableObject>
-	GLvoid Draw(const DrawableObject& object) throw() 
-	{	
-		// Clear the color and depth buffers
-		glClear(GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT);
+		/** Function that draws an OpenGL object to the screen.
+		 * Currently this is slow and badly designed but I'm going for functional
+		 * over performance to start.
+		 */
+		template <class DrawableObject>
+		GLvoid Draw(const DrawableObject& object) throw() 
+		{	
+			// Clear the color and depth buffers
+			glClear(GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT);
 
-		glLoadIdentity();											// Reset the view
-		glTranslatef(object.pos.x, object.pos.y, object.pos.z);		// Move to object location
-		glRotatef(obj.rot.x, 1.0f, 0.0f, 0.0f);						// Rotate about x-axis
-		glRotatef(obj.rot.y, 0.0f, 1.0f, 0.0f);						// Rotate about y-axis
-		glRotatef(obj.rot.z, 0.0f, 0.0f, 1.0f);						// Rotate about z-axis
+			glLoadIdentity();											// Reset the view
+			glTranslatef(object.pos.x, object.pos.y, object.pos.z);		// Move to object location
+			glRotatef(obj.rot.x, 1.0f, 0.0f, 0.0f);						// Rotate about x-axis
+			glRotatef(obj.rot.y, 0.0f, 1.0f, 0.0f);						// Rotate about y-axis
+			glRotatef(obj.rot.z, 0.0f, 0.0f, 1.0f);						// Rotate about z-axis
 
-		// Now the texture to be used needs to be set. Each drawable objects
-		// should have a texture
-		// glBindTexture(X,Y)
-		
-		// Set the color of the object to draw
-		glColor4f(object.color.x, object.color.y, object.color.z, object.color.w);
+			// Now the texture to be used needs to be set. Each drawable objects
+			// should have a texture
+			glBindTexture(GL_TEXTRURE_2D, object.texture);
+			
+			// Set the color of the object to draw
+			glColor4f(object.color.x, object.color.y, object.color.z, object.color.w);
 
-		// 
-		
+			// Call the object's list function 
+			glCallList(object.list);
 
-	}
-
+			// Double buffered so swap the buffers
+			glutSwapBuffers();	
+		}
 	}	// End namespace rend
 }		// End namespace nuke
