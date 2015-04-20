0c8b302507ca1fa97a858a484529c990288c2a1b
robclu <robclu1818@gmail.com>
2015-04-21 01:02:53 +0200

    Cleaned.

--- src/prep/other.cpp
+++ src/prep/other.cpp
@@ -53,109 +53,6 @@ typedef struct Image {
 	char *data;
 } Image;
 
-/*
-// Other bitmap loader not working
-static unsigned int getint(FILE *fp)
-{
-	int c, c1, c2, c3;
-
-	// Get 4 bytes
-	c  = getc(fp);
-	c1 = getc(fp);
-	c2 = getc(fp);
-	c3 = getc(fp);
-
-	// Return the result
-	return ((unsigned int) c ) + 
-		   (((unsigned int) c1) << 8) +
-		   (((unsigned int) c2) << 16) +
-		   (((unsigned int) c3) << 24);
-}
-
-static unsigned short getshort(FILE *fp)
-{
-	int c, c1;
-
-	// Get 2 bytes
-	c  = getc(fp);
-	c1 = getc(fp);
-
-	// Return the result
-	return ((unsigned int) c) +
-		   (((unsigned int) c1) << 8);
-}
-
-// Simple bitmap loader
-// See http://www.dcs.ed.ac.uk/~mxr/gfx/2d/BMP.txt for more info.
-int loadImage(const char * filename, Image * image)
-{
-	FILE *file;									// File handle
-	unsigned long size;							// Image size (in bytes)
-	unsigned long i;							// Counter
-	unsigned short int planes;					// Number of planes in image (must be 1)
-	unsigned short int bpp;						// Bits per pixel (must be 24)
-	char temp;									// Temp col storage for bgr->rgb conversion
-
-	// Make sure that the file exists
-	if ((file = fopen(filename, "rb")) == NULL) {
-		printf("File Not Found : %s\n", filename);
-		return 0;
-	}
-	
-	// Seek through the BMP header
-	fseek(file, 18, SEEK_CUR);
-
-	// Read the width
-    image->sizeX = getint (file);
-    printf("Width of %s: %lu\n", filename, image->sizeX);
-    
-    // Read the height 
-    image->sizeY = getint (file);
-    printf("Height of %s: %lu\n", filename, image->sizeY);
-
-	// Calculate the size (assuming 24 bits and 3 bytes per pixel
-	size = image->sizeX * image->sizeY * 3;
-
-    // Read the planes
-    planes = getshort(file);
-    if (planes != 1) {
-	printf("Planes from %s is not 1: %u\n", filename, planes);
-	return 0;
-    }
-
-    // Read the bpp
-    bpp = getshort(file);
-    if (bpp != 24) {
-      printf("Bpp from %s is not 24: %u\n", filename, bpp);
-      return 0;
-    }
-
-	// Seek past the rest of the bitmap header
-	fseek(file, 24, SEEK_CUR);
-
-	// Read in the pixel data 
-	image->data = (char *)malloc(size);			// Allocate some space
-	if (image->data == NULL) {
-		printf("Error allocating the memory for the color-corrected image data");
-		return 0;
-	}
-
-	if ((i = fread(image->data, size, 1, file)) != 1) {
-		printf("Error reading the image data from %s.\n", filename);
-		return 0;
-	}
-
-	// Reverse all if the colors (bgr -> rgb)
-	for (i = 0; i < size; i+=3) {				// Each pixel
-		temp = image->data[i];					// blue as temp
-		image->data[i] = image->data[i+2];		// Blue to red
-		image->data[i+2] = temp;				// Red to blue
-	}
-
-	// Success =D
-	return 1;
-}
-*/
 // Load the bitmaps and convert them to textures
 void loadGlTextures()
 {
@@ -177,7 +74,6 @@ void loadGlTextures()
 void initGL(int width, int height)
 {
 	loadGlTextures();							// Load the textures
-	//buildLists();								// Build the lists
 	BuildCubeList(cubeSize, &cube);				//
 	glEnable(GL_TEXTURE_2D);					// Enable texture mapping
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// Black background
