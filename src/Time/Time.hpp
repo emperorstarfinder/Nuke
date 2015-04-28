e628f5339e9a66aa9482c31687cb52bb4564e20d
robclu <robclu1818@gmail.com>
2015-04-29 00:18:00 +0200

    Added static time.

--- src/Time/Time.hpp
+++ src/Time/Time.hpp
@@ -28,8 +28,8 @@
 using namespace std::chrono;
 
 typedef high_resolution_clock				Clock;
-typedef high_resioution_clock::time_point	Time;
-typedef microseconds						Microsecs
+typedef high_resolution_clock::time_point	Time;
+typedef unsigned long long					ullong;
 
 namespace gfx {
 	namespace time {
@@ -44,46 +44,87 @@ namespace gfx {
 		 */
 		class gfxTime 
 		{
-			public:
-				static Time			startTime;		// Time at the start of the program
-				static Time			prevTime;		// Time of last iteration
-				static Microsecs	elapsedTime;	// Time since last iteration
+			private:
+				static Time startTime;	// Time at the start of the program
+				static Time	prevTime;	// Time of last iteration
 			public:
 				/*
 				 * ==============================================================
 				 * Function			: gfxTime
 				 *
-				 * Description		: Constructs the gfxTime class and sets the 
-				 *					  start time.
+				 * Description		: Constructs the gfxTime class.
 				 * ==============================================================
 				 */
-				explicit gfxTime() throw() :
-					startTime( Clock::now() ) {}
+				explicit gfxTime() throw() {}
+
+				/* 
+				 * ==============================================================
+				 * Function			: ResetTime
+				 *
+				 * Description		: Resets all the time variables to the
+				 *					  current time.
+				 * ==============================================================
+				 */
+				inline void ResetTime() throw() {
+					startTime = Clock::now();
+					prevTime  = Clock::now();
+				}
 
 				/* 
 				 * ==============================================================
 				 * Function			: TotalTime
 				 *
 				 * Description		: Returns the total time that has passed 
-				 *					  since the start of the game.
+				 *					  since the start of the program in
+				 *					  nanoseconds
 				 * ==============================================================
 				 */
-				inline Microsecs TotalTime() throw() const {
-					return duration_cast<Microsecs>( Clock::now() - startTime ).count();
+				inline ullong TotalTime() const throw() {
+					return duration_cast<nanoseconds>( Clock::now() - startTime ).count();
+				}
+
+				/*
+				 * ==============================================================
+				 * Function			: TotalTimef
+				 *
+				 * Description		: Returns the total time that has passed
+				 *					  since the time was reset, in seconds.
+				 * ==============================================================
+				 */
+				inline float TotalTimef() const throw() {
+					return ( static_cast<float>( 
+								duration_cast<nanoseconds>( 
+									Clock::now() - startTime ).count() ) / 10000000000.f );
 				}
 
 				/*
 				 * ===============================================================
-				 * Function			: ElapsedTime 
+				 * Function			: DeltaTime 
 				 *
-				 * Description		: Returns the number of microseconds since
-				 *					  the last call to ElapsedTime.
+				 * Description		: Returns the number of nanoseconds since
+				 *					  the last call to ( DeltaTime | 
+				 *					  DeltaTimef ).
 				 * ================================================================
 				 */
-				inline Microsecs ElapsedTime() throw() {
-					elapsedTime = duration_cast<Microsecs>( Clock::now() - prevTime );
-					prevTime    = Clock::now();		// Next iter prev time is this time
-					return elapsedTime;
+				inline ullong DeltaTime() throw() {
+					ullong iterTime = duration_cast<nanoseconds>( Clock::now() - prevTime ).count();
+					prevTime = Clock::now();		// Next iter prev time is this time
+					return iterTime;
+				}
+
+				/* 
+				 * ================================================================
+				 * Function			: DeltaTimef
+				 *
+				 * Description		: Returns the time since the last call to 
+				 *					  ( DeltaTimef | DeltaTime ) in secs
+				 * ================================================================
+				 */
+				inline float DeltaTimef() throw() {
+					float iterTime = static_cast<float>( duration_cast<nanoseconds> (
+								Clock::now() - prevTime ).count() ) / 1000000000.f;
+					prevTime = Clock::now();
+					return iterTime;
 				}
 		};
 	}			// End namespace time
